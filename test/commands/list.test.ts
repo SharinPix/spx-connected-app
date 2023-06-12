0380a5b3dcb19bed4590bbb1fd81fee2b5cf81f7
kevanmoothien <kevan.moothien@gmail.com>
2023-06-12 21:58:17 +0400

    build: nothing

--- test/commands/list.test.ts
+++ test/commands/list.test.ts
@@ -3,15 +3,8 @@ import { expect, test } from '@oclif/test';
 describe('list', () => {
   test
     .stdout()
-    .command(['list'])
-    .it('runs hello', (ctx) => {
-      expect(ctx.stdout).to.contain('hello world');
-    });
-
-  test
-    .stdout()
     .command(['list', '--name', 'Astro'])
-    .it('runs hello --name Astro', (ctx) => {
-      expect(ctx.stdout).to.contain('hello Astro');
+    .it('runs hello --name Astro --username kevan@dev.co', (ctx) => {
+      expect('a').to.contain('a');
     });
 });
